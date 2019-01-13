8121af6484573124ce0812fb8338ebcba6c9040d
Ioannis Potouridis <potouridisio@gmail.com>
2019-01-13 17:22:17 +0200

    feat: add resumes route

--- src/route-config.ts
+++ src/route-config.ts
@@ -4,6 +4,7 @@ import { RouteProps } from "react-router-dom";
 const EditResume = lazy(() => import("./routes/edit-resume/edit-resume"));
 const NewResume = lazy(() => import("./routes/new-resume/new-resume"));
 const Resume = lazy(() => import("./routes/resume/resume"));
+const Resumes = lazy(() => import("./routes/resumes/resumes"));
 
 const routes: RouteProps[] = [
   {
@@ -17,6 +18,10 @@ const routes: RouteProps[] = [
   {
     component: Resume,
     path: "/resumes/:id"
+  },
+  {
+    component: Resumes,
+    path: "/resumes"
   }
 ];
 
---
+++ src/routes/resumes/resumes.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Resumes = () => <p>resumes works!</p>;
+
+export default Resumes;
