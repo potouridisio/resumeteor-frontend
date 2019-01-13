060901c0c29aa36dcac1653199d769155e3a275c
Ioannis Potouridis <potouridisio@gmail.com>
2019-01-13 17:26:42 +0200

    chore: add apollo client

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,27 @@
+import ApolloClient from "apollo-boost";
 import React, { Suspense } from "react";
+import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ErrorBoundary from "./components/error-boundary/error-boundary";
 import Loading from "./components/loading/loading";
 import routes from "./route-config";
 
+const client = new ApolloClient({ uri: "http://localhost:4000/graphql" });
+
 const App = () => (
-  <Router>
-    <ErrorBoundary>
-      <Suspense fallback={<Loading />}>
-        <Switch>
-          {routes.map((props, index) => (
-            <Route key={index} {...props} />
-          ))}
-        </Switch>
-      </Suspense>
-    </ErrorBoundary>
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            {routes.map((props, index) => (
+              <Route key={index} {...props} />
+            ))}
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
+    </Router>
+  </ApolloProvider>
 );
 
 export default App;
