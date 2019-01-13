47584a91789ada34999fe746883e684adb7e8ef8
Ioannis Potouridis <potouridisio@gmail.com>
2019-01-13 11:56:16 +0200

    chore: ensure browsers render all elements consistently and in line with modern standards

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,24 @@
-import React, { Suspense } from "react";
+import React, { Fragment, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ErrorBoundary from "./components/error-boundary/error-boundary";
 import Loading from "./components/loading/loading";
+import Normalize from "./components/normalize/normalize";
 import routes from "./route-config";
 
 const App = () => (
   <Router>
-    <ErrorBoundary>
-      <Suspense fallback={<Loading />}>
-        <Switch>
-          {routes.map((props, index) => (
-            <Route key={index} {...props} />
-          ))}
-        </Switch>
-      </Suspense>
-    </ErrorBoundary>
+    <Fragment>
+      <Normalize />
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            {routes.map((props, index) => (
+              <Route key={index} {...props} />
+            ))}
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
+    </Fragment>
   </Router>
 );
 
